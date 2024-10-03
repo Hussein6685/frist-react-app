63a1a50643a990c70cfe5d306d75059428cac132
Hussein6685 <113841959+Hussein6685@users.noreply.github.com>
2024-10-03 22:25:39 +0300

    new tow input

--- src/myFrom.js
+++ src/myFrom.js
@@ -2,11 +2,16 @@ import { useState } from "react";
 export default function MyForm() {
     const [nameInput, setNameInput] = useState("");
     const [emailInput, setEmailInput] = useState("");
-    const [formInput, setFormInput] = useState({name: "", email: "" , age: ""});
+    const [formInput, setFormInput] = useState({
+        name: "",
+        email: "",
+        age: "",
+        generalInfo: "",
+        isStudent: true,
+    });
     return (
         <form
             onSubmit={(event) => {
-
                 event.preventDefault();
                 // console.log(nameInput , emailInput);
             // const formData = {
@@ -23,8 +28,10 @@ export default function MyForm() {
                 setFormInput({...formInput, name: event.target.value});
 
                 }} />
+
             <hr>
             </hr>
+
             <label>
                 your email:
             </label>
@@ -37,7 +44,10 @@ export default function MyForm() {
                 setFormInput({...formInput, email: event.target.value});
                 }}
             />
-            <hr></hr>
+
+            <hr>
+            </hr>
+
             <label>
                 your Age:
             </label>
@@ -49,6 +59,36 @@ export default function MyForm() {
             <hr>
             </hr>
 
+            <label>Are you student</label>
+            <input type="checkbox"
+                checked={formInput.isStudent}
+                value={formInput.age}
+                onChange={(event) => {
+                    setFormInput({...formInput, isStudent: event.target.checked})
+                    // if (
+                    // event.target.checked == "on"
+                    // )
+                    // {
+
+                    //     setFormInput({ ...formInput, isStudent: true });
+                    // } else {
+                    //     setFormInput({ ...formInput, isStudent: false });
+
+                    //     }
+                }}
+            />
+
+            <hr>
+            </hr>
+
+            <label>
+                General Info
+            </label>
+            <textarea onChange={(event) => {
+                setFormInput({ ...formInput, generalInfo: event.target.value });
+            }}>
+                {formInput.generalInfo}
+            </textarea>
 
             <button type="submit">Submit</button>
         </form>
